096e59a18a049255c38cef66807acfb52c555433
Gabriele Cimador <gab.cima22@gmail.com>
2025-04-10 12:03:49 +0200

    Now multiple kernels can be update at once and only sync reco is measured

--- benchmark_backend/benchmarkBackend.py
+++ benchmark_backend/benchmarkBackend.py
@@ -14,7 +14,7 @@ class BenchmarkBackend:
             "rocprofv2", "--hip-activity", "-d", self.output_folder, 
             "-o", "times_raw", "./ca", 
             "-e", f"o2-{beamtype}-{IR}Hz-128", 
-            "--syncAsync", "-g", "--memSize", "30000000000", 
+            "--sync", "-g", "--memSize", "30000000000", 
             "--preloadEvents", "--runs", "2", 
             "--RTCdeterministic", "1", "--RTCenable", "1", 
             "--RTCcacheOutput", "1", "--RTCTECHloadLaunchBoundsFromFile", "parameters.out"
@@ -32,36 +32,32 @@ class BenchmarkBackend:
         else:
             raise FileNotFoundError(f"The directory '{self.output_folder}' does not exist.")
 
-    def update_param_file(self, kernel_name, block_size, grid_size, filename="include/testParam.h"):
-        sed_command = (
-            f"sed -i '/^\\s*#define GPUCA_LB_GPUTPC{kernel_name} \\s*[0-9][0-9]*,\\? *[0-9]*/"
-            f"{{s/\\s[0-9][0-9]*,\\? *[0-9]*/ {block_size}, {grid_size//60}/}}' {filename}"
-        )
-        os.system(sed_command)
+    def update_param_file(self, kernels_config, filename="include/testParam.h"):
+        for kernel_name, config in kernels_config.items():
+            block_size = config["block_size"]
+            grid_size = config["grid_size"]
+            sed_command = (
+                f"sed -i '/^\\s*#define GPUCA_LB_GPUTPC{kernel_name} \\s*[0-9][0-9]*,\\? *[0-9]*/"
+                f"{{s/\\s[0-9][0-9]*,\\? *[0-9]*/ {block_size}, {grid_size//60}/}}' {filename}")
+            os.system(sed_command)
         root_command = "root -l -q -b src/GPU/GPUTracking/Standalone/tools/dumpGPUDefParam.C"
         log_file = os.path.join(self.output_folder, 'time_kernels.log')
         with open(log_file, 'a') as f:
             subprocess.run(root_command, shell=True, stdout=f, stderr=subprocess.STDOUT)
 
     def _compute_durations(self, search_string):
-        # Open the input CSV file
         input_file = f'{self.output_folder}/times_raw.csv'
         output_file = f'{self.output_folder}/{search_string}.csv'
         with open(input_file, 'r') as infile:
             reader = csv.DictReader(infile)
-            # Prepare data for the output
             results = []
             for row in reader:
                 kernel_name = row["Kernel_Name"]
-                # Filter kernel names containing the search string
                 if search_string in kernel_name:
                     start_time = int(row["Start_Timestamp"])
                     stop_time = int(row["Stop_Timestamp"])
-                    # Calculate the duration in microseconds
                     duration = (stop_time - start_time) / 1000000.0
-                    results.append({"Kernel_Name": kernel_name, "Duration (ms)": duration})
-        
-        # Write the results to the output CSV
+                    results.append({"Kernel_Name": kernel_name, "Duration (ms)": duration})        
         with open(output_file, 'w', newline='') as outfile:
             fieldnames = ["Kernel_Name", "Duration (ms)"]
             writer = csv.DictWriter(outfile, fieldnames=fieldnames)
@@ -75,6 +71,26 @@ class BenchmarkBackend:
             if not file_exists:
                 writer.writerow(fieldnames)
             writer.writerows(rows)
+
+    def _compute_step_duration(self, step_name, kernels_config):
+        input_file = f'{self.output_folder}/times_raw.csv'
+        output_file = f'{self.output_folder}/{step_name}.csv'
+        step_kernels = [key for key in kernels_config.keys()]
+        with open(input_file, 'r') as infile:
+            reader = csv.DictReader(infile)
+            results = []
+            for row in reader:
+                kernel_name = row["Kernel_Name"]
+                if step_name in step_kernels:
+                    start_time = int(row["Start_Timestamp"])
+                    stop_time = int(row["Stop_Timestamp"])
+                    duration = (stop_time - start_time) / 1000000.0
+                    results.append({"Kernel_Name": kernel_name, "Duration (ms)": duration})        
+        with open(output_file, 'w', newline='') as outfile:
+            fieldnames = ["Kernel_Name", "Duration (ms)"]
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(results)
 
     def _compute_kernel_mean(self, kernel_name, block_size, grid_size, beamtype, IR, write_to_csv=True):
         input_file = os.path.join(self.output_folder, f'{kernel_name}.csv')
@@ -117,31 +133,37 @@ class BenchmarkBackend:
             reader = csv.reader(infile)
             next(reader)
             data = [float(row[1]) for row in reader]
-        labels = [("sync", 1), ("sync", 2), ("async", 1), ("async", 2)]
         results = []
         stats = []
-        for i, (proc_type, merger_idx) in enumerate(labels):
-            subset = data[i::4]
+        for i, idx in enumerate(["1", "2"]):
+            subset = data[i::2]
             mean = np.mean(subset)
             std_dev = np.std(subset)
-            row = [block_size, grid_size, beamtype, IR, proc_type, merger_idx, mean, std_dev]
+            row = [block_size, grid_size, beamtype, IR, idx, mean, std_dev]
             stats.append((mean, std_dev))
             results.append(row)
         if write_to_csv:
-            self._write_stats_to_csv(output_file, ["block_size", "grid_size", "beamtype", "IR", "proc_type", "merger_idx", "mean", "std_dev"], results)
+            self._write_stats_to_csv(output_file, ["block_size", "grid_size", "beamtype", "IR", "idx", "mean", "std_dev"], results)
         return stats[0]
 
     def _compute_mean_times(self, kernel_name, block_size, grid_size, beamtype, IR):
-        # Check which kernel to process
-        if "GMMergerCollect" == kernel_name:
-            return self._compute_MergerCollect_mean(block_size, grid_size, beamtype, IR)
-        elif "GMMergerTrackFit" == kernel_name:
+        if "GMMergerTrackFit" == kernel_name:
             return self._compute_MergerTrackFit_mean(block_size, grid_size, beamtype, IR)
         else:
             return self._compute_kernel_mean(kernel_name, block_size, grid_size, beamtype, IR)
 
     def get_kernel_mean_time(self, kernel_name, block_size, grid_size, beamtype, IR):
-        self.update_param_file(kernel_name, block_size, grid_size)
+        kernles_config = {kernel_name: {"block_size": block_size, "grid_size": grid_size}}
+        self.update_param_file(kernles_config)
         self.profile_benchmark(beamtype, IR)
         self._compute_durations(kernel_name)        
         return self._compute_mean_times(kernel_name, block_size, grid_size, beamtype, IR)
+
+    def get_step_mean_time(self, step_name, kernels_config, beamtype, IR):
+        self.update_param_file(kernels_config)
+        self.profile_benchmark(beamtype, IR)
+        for kernel_name in kernels_config.keys():
+            self._compute_durations(kernel_name)
+        self._compute_step_duration(step_name, kernels_config)
+        return self._compute_mean_times("GMMergerCollect", 0, 0, beamtype, IR), \
+               self._compute_mean_times("GMMergerTrackFit", 0, 0, beamtype, IR)
